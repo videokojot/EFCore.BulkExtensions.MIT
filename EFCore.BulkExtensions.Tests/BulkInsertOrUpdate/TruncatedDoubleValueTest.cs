4bebe7b19ee0d05ae22a034adb40d28fadf8a39a
videokojot <jindrich.cincibuch@gmail.com>
2023-11-29 07:40:32 +0100

    WIP

--- EFCore.BulkExtensions.Tests/BulkInsertOrUpdate/TruncatedDoubleValueTest.cs
+++ EFCore.BulkExtensions.Tests/BulkInsertOrUpdate/TruncatedDoubleValueTest.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Xunit;
 
